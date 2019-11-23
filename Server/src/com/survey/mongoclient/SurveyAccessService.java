c21206100e86efc09e068463b8dd07237033b6c7
Dulanjan Madusanka <53951683+Madusanka-47@users.noreply.github.com>
2019-11-23 14:10:04 +0530

    Encryption implementation@dumalk

--- Server/src/com/survey/mongoclient/SurveyAccessService.java
+++ Server/src/com/survey/mongoclient/SurveyAccessService.java
@@ -22,29 +22,29 @@ public class SurveyAccessService {
             MongoCollection<org.bson.Document> collection = database.getCollection(collectionName);
             FindIterable<org.bson.Document> sprusr = collection.find(eq("superuser", true));
             List<Document> newUser = new ArrayList<Document>();
+            String userid = Long.toString(collection.countDocuments() + 1);
 
             for (org.bson.Document spruser : sprusr.collation(null)) {
                 if (spruser.get("username").equals(currntUserName)) {
                     if (!usrname.isEmpty() && !pswd.isEmpty()) {
                         Document isAvilable = collection.find(eq("username", usrname)).first();
                         if (isAvilable == null) {
-                            newUser.add(new Document("userid", collection.countDocuments() + 1)
-                                    .append("username", usrname).append("password", pswd).append("superuser", isSuper));
-                            collection.insertMany(newUser);
-                            return 1;
+                            SurveyLogingService secure = new SurveyLogingService();
+                            String hashcode = secure.encryptLogings(pswd, userid, null, true);
+                            if (hashcode != null) {
+                                newUser.add(new Document("userid", userid).append("username", usrname)
+                                        .append("password", hashcode).append("superuser", isSuper));
+                                collection.insertMany(newUser);
+                                return 1;
+                            } else {
+                                throw new Exception("Failed hashed");
+                            }
                         } else {
                             throw new Exception("User name alredy exist");
                         }
                     }
                 }
             }
-            // long quseId = collection.countDocuments();
-            // List<Document> questionSet = new ArrayList<Document>();
-            // questionSet.add(new Document("quesid", quseId + i).append("description",
-            // questions.get(i))
-            // .append("ques_type", Arrays.asList(options.get(i))).append("basic", false));
-
-            // collection.insertOne(questionSet);
         } catch (Exception ex) {
             System.out.println(ex);
             return 0;
@@ -62,6 +62,6 @@ public class SurveyAccessService {
 class StartAccessService {
     public static void main(String[] args) {
         SurveyAccessService acc = new SurveyAccessService();
-        acc.createPaneUser("admin", "dulanjan", "1235", false);
+        acc.createPaneUser("admin", "Madusanka47", "Name128$", false);
     }
 }
--- Server/src/com/survey/mongoclient/SurveyLogingService.java
+++ Server/src/com/survey/mongoclient/SurveyLogingService.java
@@ -1,5 +1,93 @@
 package com.survey.mongoclient;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
+import static com.mongodb.client.model.Filters.eq;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
+import org.bson.types.Binary;
+
 public class SurveyLogingService {
-    
+    private static final String hashMap = "access_keys";
+
+    public String encryptLogings(String usp, String usrid, byte[] s4lt, boolean isStore)
+            throws NoSuchProviderException {
+        String hashcode = null;
+        try {
+            byte[] salt;
+            if (s4lt.length < 0) {
+                SecureRandom sr = SecureRandom.getInstance("SHA1PRNG", "SUN");
+                salt = new byte[16];
+                sr.nextBytes(salt);
+            } else {
+                salt = s4lt;
+            }
+            if (!usp.isEmpty() || usp != null) {
+                MessageDigest md = MessageDigest.getInstance("MD5");
+                md.update(salt);
+                byte[] bytes = md.digest(usp.getBytes());
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < bytes.length; i++) {
+                    sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+                }
+                hashcode = sb.toString();
+                if (isStore) {
+                    hASHmAP(usrid, salt, true);
+                }
+            }
+            return hashcode;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return hashcode;
+        }
+    }
+
+    public String decryptLoggins(String usp, String usrid) {
+        try {
+            Document doc = hASHmAP(usrid, null, false);
+            final Binary binary = doc.get("hashkey", Binary.class);
+            final byte[] salt = binary.getData();
+            return encryptLogings(usp, usrid, salt, false);
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+            return null;
+        }
+    }
+
+    public Document hASHmAP(String userid, byte[] salt, boolean isStore) {
+        Document hashkey = null;
+        try {
+            MongoConnector dbo = new MongoConnector();
+            MongoDatabase database = dbo.getConnection();
+            MongoCollection<org.bson.Document> collection = database.getCollection(hashMap);
+            if (!userid.isEmpty()) {
+                if (isStore) {
+                    collection.insertOne(new Document("userid", userid).append("hashkey", salt));
+                } else {
+                    hashkey = collection.find(eq("userid", userid)).first();
+                }
+            }
+        } catch (Exception ex) {
+
+        }
+        return hashkey;
+    }
+}
+
+/**
+ * Debug main for SurveyQuestionService Remove once the implementaion completed
+ */
+class StartLoginService {
+    public static void main(String[] args)
+            throws UnsupportedEncodingException, NoSuchProviderException, NoSuchAlgorithmException {
+        SurveyLogingService slt = new SurveyLogingService();
+        System.out.println(slt.decryptLoggins("1235", "7"));
+
+    }
 }
